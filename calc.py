1da2843a0cfb6d1ca6536934894fd89c5b49267b
BenToryMonash <btor0003@student.monash.edu>
2026-04-01 21:54:52 +1100

    Ammend wrong quotes

--- calc.py
+++ calc.py
@@ -1,17 +1,17 @@
 # Perform simple arithmetic encoded in an input string:
 # '1 + 2' -> 3, or '1 - 2' -> -1.
 def compute(expression):
-    num0, operator, num1 = expression.split(" ")
+    num0, operator, num1 = expression.split(' ')
     num0, num1 = float(num0), int(num1)
-    if operator == "+":
+    if operator == '+':
         return num0 + num1
-    elif operator == "-":
+    elif operator == '-':
         return num0 - num1
-    elif operator == "*":
+    elif operator == '*':
         return num0 * num1
-    elif operator == "/":
+    elif operator == '/':
         return num0 / num1
     else:
-        print("unknown operator!")
-        return None
+        print('unknown operator!')
+	return None
 
